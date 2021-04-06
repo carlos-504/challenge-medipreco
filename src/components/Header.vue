8d31f0ac4c47a3a4fae3bda223780439b61e296a
carlos-504 <carllosshennriique@gmail.com>
2021-04-06 14:14:17 -0300

    feat: add home page and consume api

---
+++ src/components/Header.vue
@@ -0,0 +1,104 @@
+<template>
+    <header class="header-container">
+        <div class="logo-container">
+            <p class="logo"><span class="medi-logo">Medi</span>Drinks</p>
+            <div class="loc-container">
+                <LocSVG class="loc-icon" />
+                <p class="localization">
+                    Brasília, DF
+                </p>
+            </div>
+        </div>
+        <div class="github">
+            <p class="user-github">
+                <b-icon-github class="icon-git" /> {{ userGitHub }}
+            </p>
+        </div>
+    </header>
+</template>
+
+<script>
+import LocSVG from './LocIcon.vue';
+
+export default {
+    data() {
+        return {
+            userGitHub: '@carlos-504',
+        };
+    },
+
+    components: {
+        LocSVG,
+    },
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+.header-container {
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid rgba(10, 23, 55, 0.1);
+    padding-left: 20px;
+    font-family: 'Inter', sans-serif;
+}
+
+.github {
+    align-self: center;
+    margin-right: 20px;
+}
+
+.icon-git {
+    margin-right: 10px;
+}
+
+.user-github {
+    border: 1px solid rgba(10, 23, 55, 0.1);
+    border-radius: 5px;
+    padding: 7px 18px;
+    font-family: Inter;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 24px;
+    text-align: center;
+    letter-spacing: -0.006em;
+    color: #252c32;
+}
+
+.logo {
+    margin-bottom: 0px;
+    margin-top: 20px;
+    letter-spacing: -0.014em;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 24px;
+}
+
+.medi-logo {
+    color: #4094f7;
+}
+
+.logo-container {
+    text-align: left;
+    align-items: center;
+    margin-left: 20px;
+}
+
+.loc-container {
+    display: flex;
+    margin-top: -6px;
+}
+
+.loc-icon {
+    margin-right: 10px;
+    margin-bottom: 5px;
+    align-self: center;
+}
+
+.localization {
+    margin-top: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,158 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <Header />
+        <div class="top-container">
+            <div class="typography-container">
+                <h3 class="typography">
+                    Os melhores drinks não-alcoólicos do
+                    <span class="brasil-word">Brasil</span>
+                </h3>
+                <p class="result-message">
+                    {{ drinks.length }} resultados encontrados
+                </p>
+            </div>
+            <div class="views-container">
+                <div class="view-grid"><ViewGrid /></div>
+                <div class="view-list"><ViewList /></div>
+            </div>
+        </div>
+        <div class="desgraca">
+            <div class="drinks-container">
+                <div class="img-container" v-for="drink in drinks">
+                    <img class="img-drinks" :src="drink.strDrinkThumb" />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue';
+import ViewGrid from '@/components/ViewGrid.vue';
+import ViewList from '@/components/ViewList.vue';
+
+const axios = require('axios');
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    components: {
+        Header,
+        ViewGrid,
+        ViewList,
+    },
+
+    data() {
+        return {
+            drinks: [],
+        };
+    },
+
+    methods: {
+        async getDrinks() {
+            try {
+                const drinks = await axios.get(
+                    'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+                );
+
+                this.drinks = drinks.data.drinks;
+            } catch (err) {
+                console.log(err);
+            }
+        },
+    },
+
+    mounted() {
+        this.getDrinks();
+    },
+};
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+.brasil-word {
+    color: #4094f7;
+}
+
+.top-container {
+    display: flex;
+    justify-content: space-between;
+    margin: 17px 82px;
+}
+
+.typography-container {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+}
+
+.typography {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 32px;
+}
+
+.result-message {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 32px;
+    letter-spacing: -0.019em;
+    color: #9aa6ac;
+    margin-top: -10px;
+}
+
+.views-container {
+    display: flex;
+    align-self: flex-start;
+}
+
+.view-grid,
+.view-list {
+    padding: 7px 9px;
+    background: #f6f8f9;
+}
+
+.view-grid {
+    border-radius: 6px 0px 0px 6px;
+}
+
+.view-list {
+    border-radius: 0px 6px 6px 0px;
+}
+
+.view-grid:active,
+.view-list:active {
+    background: #e5e9eb;
+}
+
+.drinks-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: 0px 2px;
+    grid-template-areas:
+        '. . .'
+        '. . .'
+        '. . .';
+    width: 67%;
+    height: auto;
+    justify-content: center;
+}
+
+.desgraca {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+}
+
+.img-container {
+    margin: 0 auto;
+}
+
+.img-drinks {
+    width: 100%;
+    height: auto;
+}
+</style>
